b8d3cfb112d4771adb83e19b8ea8a4351e5a8a38
WSL <2112873995@qq.com>
2026-01-05 00:06:03 +0800

    完善了一下日志相关配置

--- service/user/rpc/internal/config/config.go
+++ service/user/rpc/internal/config/config.go
@@ -2,6 +2,16 @@ package config
 
 import "github.com/zeromicro/go-zero/zrpc"
 
+type Postgres struct {
+	Host     string
+	Dbname   string
+	Password string
+	Port     string
+	User     string
+}
+
 type Config struct {
 	zrpc.RpcServerConf
+	Postgres Postgres
 }
+
